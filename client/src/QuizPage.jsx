c73bc1f3087c9f268c13f017c4d75f1c664400a5
Mike <jogdial25@gmail.com>
2025-03-21 19:26:58 +0200

    changed layout,added questions

--- client/src/QuizPage.jsx
+++ client/src/QuizPage.jsx
@@ -10,7 +10,7 @@ function QuizPage({ teams, setTeams }) {
     useEffect(() => {
       const getQuestions = async () => {
         try {
-          const response = await axios.get('https://quizback.webcodes.ee/quiz')
+          const response = await axios.get('https://quiz.webcodes.ee/quiz')
           setQuestions(response.data)
         } catch (error) {
           console.log(error);
@@ -50,64 +50,70 @@ function QuizPage({ teams, setTeams }) {
       if (currentQuestionIndex > 0) {
         // Reset the answer status for the current question immediately
         const newStatus = [...answerStatus];
-        newStatus[currentQuestionIndex] = [];  // Clear statuses for the current question
+        newStatus[currentQuestionIndex] = []; 
         setAnswerStatus(newStatus);
           setCurrentQuestionIndex(currentQuestionIndex - 1);
       }
   };
 
   return (
-    <div className="quiz-container">
-    <h1 className="quiz-header">Quiz App</h1>
-    <div className="team-info">
-        {teams.map((team, index) => (
-            <div key={index} className="team">
-                <strong>{team.name}</strong>: {team.score} punkti
-            </div>
-        ))}
-        <select 
-            onChange={(e) => setCurrentTeam(Number(e.target.value))} 
-            value={currentTeam} 
-            className="team-select"
-        >
-            {teams.map((team, index) => (
-                <option key={index} value={index}>{team.name}</option>
-            ))}
-        </select>
-    </div>
-    {questions.length > 0 && (
-        <div className="question-section">
-            {/* <h2 className="question-title">Küsimus {currentQuestionIndex + 1}</h2> */}
+    <div className='main'>
+        <div class="question-overlay"></div>
+        <div className='wrapper-div'>
+        {questions.length > 0 && (
             <h3 className="question-text">{questions[currentQuestionIndex].question}</h3>
-            {questions[currentQuestionIndex].answers.map((a, index) => (
-                <button 
-                key={index} 
-                onClick={() => handleAnswerClick(a.correct, index)} 
-                className={`answer-button ${answerStatus[currentQuestionIndex]?.[index] === true ? 'correct' : answerStatus[currentQuestionIndex]?.[index] === false ? 'incorrect' : ''}`}
-            >
-                {a.text}
-            </button>
-            ))}
-            <div className="navigation-buttons">
-                <button 
-                    onClick={prevQuestion} 
-                    disabled={currentQuestionIndex === 0} 
-                    className="nav-button"
-                >
-                    ◀ Prev
-                </button>
-                <span className="question-count"> {currentQuestionIndex + 1} / {questions.length} </span>
-                <button 
-                    onClick={nextQuestion} 
-                    disabled={currentQuestionIndex === questions.length - 1} 
-                    className="nav-button"
-                >
-                    Next ▶
-                </button>
+        )}
+            <div className="quiz-container">
+                <h1 className="quiz-header">Quiz App</h1>
+                <div className="team-info">
+                    {teams.map((team, index) => (
+                        <div key={index} className="team">
+                            <strong>{team.name}</strong>: {team.score} punkti
+                        </div>
+                    ))}
+                    <label htmlFor="">Vastab tiim </label>
+                    <select 
+                        onChange={(e) => setCurrentTeam(Number(e.target.value))} 
+                        value={currentTeam} 
+                        className="team-select"
+                        >
+                        {teams.map((team, index) => (
+                            <option key={index} value={index}>{team.name}</option>
+                        ))}
+                    </select>
+                </div>
             </div>
+            {questions.length > 0 && (
+                <div className="question-section">
+                    <div className="button-grid">
+                    {questions[currentQuestionIndex].answers.map((a, index) => (
+                            <button 
+                            key={index} 
+                            onClick={() => handleAnswerClick(a.correct, index)} 
+                            className={`answer-button ${answerStatus[currentQuestionIndex]?.[index] === true ? 'correct' : answerStatus[currentQuestionIndex]?.[index] === false ? 'incorrect' : ''}`}>
+                            {a.text}
+                            </button>
+                    ))}
+                    </div>
+                    <div className="navigation-buttons">
+                        <button 
+                            onClick={prevQuestion} 
+                            disabled={currentQuestionIndex === 0} 
+                            className="nav-button">
+                            ◀ Prev
+                        </button>
+                        <span className="question-count"> {currentQuestionIndex + 1} / {questions.length} </span>
+                        <button 
+                            onClick={nextQuestion} 
+                            disabled={currentQuestionIndex === questions.length - 1} 
+                            className="nav-button">
+                            Next ▶
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
-    )}
-</div>
+    </div>
   );
 }
 
